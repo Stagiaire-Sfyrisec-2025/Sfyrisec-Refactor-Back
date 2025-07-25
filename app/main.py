1f69347916cc9b0a37683d4d6d3ad068d71f2c1c
Erico06-lls <tolojanaharyjeanerico@gmail.com>
2025-07-25 10:33:16 +0300

    feat: ajout de la fonctionnalite upload & analyse de code

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from app.api.v1 import upload
+from api.v1 import upload
 
-app = FastAPI(title="Code Refactor API")
+app = FastAPI()
 
 app.include_router(upload.router, prefix="/api/v1/upload", tags=["Upload"])
---
+++ app/utils/constants.py
@@ -0,0 +1,2 @@
+EXCLUDED_DIRS = {"venv", "env", "__pycache__", ".git", "node_modules"}
+ALLOWED_EXTENSIONS = {".py"}
